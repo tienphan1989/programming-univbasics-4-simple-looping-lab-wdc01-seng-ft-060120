1b86df84228f6a3f0fe766323336ef359f6e2c7b
Tien Phan <tienphan1989@gmail.com>
2020-05-03 07:29:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,4 +3,13 @@ def loop_message_five_times(string)
   while counter < 5 do
     puts string
     counter += 1 
-    
+  end
+end
+
+def loop_message_five_times(string)
+  counter = 0 
+  while counter < 5 do
+    puts string
+    counter += 1 
+  end
+end
